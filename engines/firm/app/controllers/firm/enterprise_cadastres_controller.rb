47786128268be05ac2020f294bfc447818a113d6
Igor Marques <igormarjes@gmail.com>
2018-05-17 09:47:16 -0300

    ok

--- engines/firm/app/controllers/firm/enterprise_cadastres_controller.rb
+++ engines/firm/app/controllers/firm/enterprise_cadastres_controller.rb
@@ -23,7 +23,7 @@ module Firm
       @enterprise_excell = apply_scopes(@enterprise_cadastres)
       @enterprise_cadastres = @enterprise_excell.paginate(:page => params[:page], :per_page => 20)
       respond_to do |format|
-      format.html
+       format.html
        format.csv  { response.headers['Content-Disposition'] = "attachment; filename='candidatos#{Date.today.to_date}.xlsx'" }
       end
     end
