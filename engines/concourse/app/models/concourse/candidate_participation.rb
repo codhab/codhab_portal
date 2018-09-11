0991c34b15e32da10afb417391215afe727f9d8d
Elton Silva <elton.chrls@gmail.com>
2018-09-11 16:01:39 -0300

    ok

--- engines/concourse/app/models/concourse/candidate_participation.rb
+++ engines/concourse/app/models/concourse/candidate_participation.rb
@@ -6,11 +6,11 @@ module Concourse
     has_many  :team_participations, dependent: :destroy
     accepts_nested_attributes_for :team_participations, allow_destroy: true, reject_if: :all_blank
 
-    validates :archive_one, :archive_two, presence: true
+    validates :archive_one, :archive_two, :archive_three, presence: true
 
     validates :archive_one, file_content_type: { allow: ['application/pdf'],
                                                  message: 'Somente arquivos no formato PDF' },
-                                                 file_size: {less_than_or_equal_to: 18.megabytes.to_i,
+                                                 file_size: {less_than_or_equal_to: 30.megabytes.to_i,
                                                  message: 'O arquivo excede o tamanho permitido'}
 
 
@@ -20,9 +20,16 @@ module Concourse
                                                  message: 'O arquivo excede o tamanho permitido'}
 
 
+    validates :archive_three, file_content_type: { allow: ['image/png', 'image/jpg'],
+                                                 message: 'Somente arquivos no formato PNG ou JPG' },
+                                                 file_size: {less_than_or_equal_to: 5.megabytes.to_i,
+                                                 message: 'O arquivo excede o tamanho permitido'}
+
+
 
 
     mount_uploader :archive_one,   Concourse::FileUploader
     mount_uploader :archive_two,   Concourse::FileUploader
+    mount_uploader :archive_three, Concourse::FileUploader
   end
 end
