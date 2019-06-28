2dba12752c5863dc3b7b44d284b7d5ce765ec82b
Elton Silva <elton.chrls@gmail.com>
2019-06-28 10:53:13 -0300

    oK

--- app/models/brb_file.rb
+++ app/models/brb_file.rb
@@ -2,10 +2,13 @@ class BrbFile
 
   def self.file_return
     dir = Dir.entries("#{Rails.root}/brb_file/RECEPÇÃO")
-    
+    dir.delete '.'
+    dir.delete '..'
+    dir = dir.sort_by { |item| self.parse_date(item) } 
+
     json = []
 
-    dir.sort.each_with_index do |file, index|
+    dir.each_with_index do |file, index|
       next if index < 2
 
       json << {
@@ -13,9 +16,14 @@ class BrbFile
         'arquivo': "http://www.codhab.df.gov.br/boletos/arquivo/#{file}"
       }
     end
-
-    json
-  rescue
+    
+    json 
+  rescue Exception => e
+    p e
     json = []
+  end
+
+  def self.parse_date(item)
+    Date.parse("#{item.scan(/\d+/)[1].to_s[4..7]}#{item.scan(/\d+/)[1].to_s[2..3]}#{item.scan(/\d+/)[1].to_s[0..1]}") rescue Date.parse('19000101')
   end
 end
