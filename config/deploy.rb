9f7127550eb0ae3c7200135982c1271478f490bc
Elton Silva <elton.chrls@gmail.com>
2019-07-09 13:39:27 -0300

    OK

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ task :deploy do
     invoke  :'git:clone'
     invoke  :'deploy:link_shared_paths'
     invoke  :'bundle:install'
-    #invoke  :'rails:assets_precompile'
+    invoke  :'rails:assets_precompile'
     invoke  :'deploy:cleanup'
     on :launch do
       invoke :'unicorn:restart'
