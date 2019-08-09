ce11c112f7ce32c05fbf35b82651d64d00dfc974
Elton Silva <elton.chrls@gmail.com>
2019-08-09 10:32:46 -0300

    OK

--- engines/cpl_competition/app/models/cpl_competition/user.rb
+++ engines/cpl_competition/app/models/cpl_competition/user.rb
@@ -4,8 +4,8 @@ module CplCompetition
 
     has_many :competition_users
     
-    validates :cpf,  uniqueness: true, allow_blank: true
-    validates :cnpj, uniqueness: true, allow_blank: true
+    validates :cpf,  cpf: true, uniqueness: true, allow_blank: true
+    validates :cnpj, cnpj: true, uniqueness: true, allow_blank: true
     
     def special_name
       if administrator?
