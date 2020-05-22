58e35372e5ad5b8398fec91c6e483a6527960b6e
Elton Silva <elton.chrls@gmail.com>
2020-05-22 17:49:25 -0300

    OK

---
+++ engines/candidate/app/models/candidate/view/general_candidate.rb
@@ -0,0 +1,33 @@
+module Candidate
+  module View
+    class GeneralCandidate < ActiveRecord::Base 
+      self.table_name = 'extranet.general_candidates'
+            
+      belongs_to :cadastre,         class_name: ::Core::Candidate::Cadastre, foreign_key: :id
+      belongs_to :dependent,        class_name: ::Core::Candidate::Dependent, foreign_key: :cadastre_id
+      belongs_to :situation_status, class_name: ::Core::Candidate::SituationStatus
+
+      scope :by_income, -> (started_at = 0, ended_at = 1600) { where(income: started_at..ended_at) }
+      scope :by_cpf, -> (cpf) {
+
+        current = find_by(cpf: cpf.to_s.unformat_cpf) rescue nil
+
+        if current.nil?
+          all
+        else
+          where(total: (current.total - 0.400)..(current.total + 0.400))
+        end
+      }
+      scope :by_name, -> (name) {
+
+        name = name.strip!
+        where("name ILIKE '%#{name}%' ")
+
+      }
+
+      def cadastre
+        Core::Candidate::Cadastre.find(self.id) rescue nil
+      end
+    end
+  end
+end
