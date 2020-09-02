a81cbc4221b08e3f0c79adf219b7e3dbbd3c6eb0
Elton Silva <elton.chrls@gmail.com>
2020-09-02 16:55:32 -0300

    OK

--- engines/candidate/app/forms/candidate/crixa_indication_form.rb
+++ engines/candidate/app/forms/candidate/crixa_indication_form.rb
@@ -16,9 +16,9 @@ module Candidate
       if cadastre.nil?
         errors.add(:cpf, "CPF ou data de nascimento não conferem com os dados do cadastro na CODHAB; Faça seu agendamento diretamente nos agendamentos CODHAB, <a href='http://www.codhab.df.gov.br/agendamento/habitacao'>Clique aqui</a>".html_safe)
       else
-        schedules = ::Schedule::DataReference.where(code: 'crixa-setembro-2020').find_by(cpf: self.cpf)
+        schedules = ::Schedule::DataReference.where(code: 'crixa-setembro-2020', cpf: self.cpf)
 
-        if !schedules.nil?
+        if !schedules.present?
           errors.add(:cpf, "CPF informado não está apto para agendar atendimento. Este agendamento é direcionado para os indicados ao Crixá que ainda não formalizaram dossiê.")
         else
           self.cadastre_id = cadastre.id
