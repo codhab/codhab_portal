eb016476eaebc247fe60f46127a3c6d6e3933497
Elton Silva <elton.chrls@gmail.com>
2020-11-13 16:27:18 -0300

    Update mestre_darmas_regularizations_controller.rb

--- engines/regularization/app/controllers/regularization/mestre_darmas_regularizations_controller.rb
+++ engines/regularization/app/controllers/regularization/mestre_darmas_regularizations_controller.rb
@@ -2,6 +2,7 @@ require_dependency 'regularization/application_controller'
 
 module Regularization
   class MestreDarmasRegularizationsController < ApplicationController
+    before_action :allow_iframe
     
     def index
     end
@@ -33,6 +34,10 @@ module Regularization
     def set_params
       params.require(:mestre_darmas_regularization)
         .permit(:name, :address, :cpf, :rg, :rg_emission_date, :rg_org, :civil_state_id, :income, :nationality, :contact_phone, :contact_mobile, :contact_email, :spouse_name, :spouse_cpf, :spouse_rg, :spouse_rg_emission_date, :spouse_rg_org, :declaration_descriptive, :declaration_pacific, :declaration_spouse_pacific, :declaration_stable_union, :declaration_informal_activity, :declaration_informal_activity_value, :declaration_spouse_informal_activity, :declaration_spouse_informal_activity_value, :terms_use, :income_document, :cpf_document, :rg_document, :civil_state_document, :document_address, :document_iptu, :document_onus, :spouse_rg_document, :spouse_cpf_document)
+    end
+    
+    def allow_iframe
+      response.headers.delete('X-Frame-Options')
     end
 
   end
